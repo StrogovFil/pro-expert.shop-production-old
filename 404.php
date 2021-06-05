c6f17038c0514ec255188e3d5ef0ddd3e10e2651
Igor <igorekimow@gmail.com>
2021-06-05 20:59:33 +0300

    Fix .htaccess

--- 404.php
+++ 404.php
@@ -8,6 +8,13 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Страница не найдена");
 $APPLICATION->SetPageProperty("keywords", "Страница не найдена");
 $APPLICATION->SetPageProperty("description", "Страница не найдена");
-?><section>
-<div class="container"><h1>404</h1></div>
-</section><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+<section>
+    <div class="container">
+        <h1>404</h1>
+        <p>Произошла ошибка</p>
+        <p>Запрашиваемая страница не найдена!</p>
+        <p>Воспользуйтесь меню сайта, чтобы попасть в нужный вам раздел.</p>
+    </div>
+</section>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
