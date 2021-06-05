0559c4db44dfdfdc8c69167cf501b611712bca03
Igor <igorekimow@gmail.com>
2021-06-06 00:14:06 +0300

    Fix index transition

--- local/templates/theexpert/includes/index/catalog.php
+++ local/templates/theexpert/includes/index/catalog.php
@@ -2,62 +2,62 @@
         <div class="main-rubricator-inner container">
             <div class="main-rubricator-cell">
                 <a href="/catalog/place-ofisy/" class="main-rubricator-item main-rubricator-item-red" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Офисные пространства</div>
+                    <div class="main-rubricator-item-title"><span>Офисные пространства</span></div>
                 </a>
             </div>
             <div class="main-rubricator-cell">
                 <a href="/catalog/place-oteli-i-apartamenty/" class="main-rubricator-item main-rubricator-item-top main-rubricator-item-dark" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Отели <br> и апартаменты</div>
+                    <div class="main-rubricator-item-title"><span>Отели <br> и апартаменты</span></div>
                 </a>
                  <a href="/catalog/place-zagorodnye-doma/" class="main-rubricator-item main-rubricator-item-bottom" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Загородные дома</div>
+                    <div class="main-rubricator-item-title"><span>Загородные дома</span></div>
                 </a>
             </div>
             <div class="main-rubricator-cell">
                 <a href="/catalog/place-sportivnye-uchrezhdeniya-i-stadiony/" class="main-rubricator-item main-rubricator-item-top" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Спортивные учреждения <br>и стадионы</div>
+                    <div class="main-rubricator-item-title"><span>Спортивные учреждения <br>и стадионы</span></div>
                 </a>
                 <a href="/catalog/place-meditsinskie-uchrezhdeniya-i-spa-salony/" class="main-rubricator-item main-rubricator-item-bottom main-rubricator-item-red" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Медицинские учреждения <br>и SPA салоны</div>
+                    <div class="main-rubricator-item-title"><span>Медицинские учреждения <br>и SPA салоны</span></div>
                 </a>
             </div>
             <div class="main-rubricator-cell">
                 <a href="/catalog/place-skladskaya-i-proizvodstvennaya-nedvizhimost/" class="main-rubricator-item main-rubricator-item-top" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Складская <br>и производственная <br>недвижимость</div>
+                    <div class="main-rubricator-item-title"><span>Складская <br>и производственная <br>недвижимость</span></div>
                 </a>
                 <a href="/catalog/place-shou-rumy-i-butiki/" class="main-rubricator-item main-rubricator-item-bottom main-rubricator-item-white" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Торговая недвижимость</div>
+                    <div class="main-rubricator-item-title"><span>Торговая недвижимость</span></div>
                 </a>
             </div>
             <div class="main-rubricator-cell">
                 <a href="/catalog/place-kongress-tsentry/" class="main-rubricator-item main-rubricator-item-dark" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Конгресс-центры</div>
+                    <div class="main-rubricator-item-title"><span>Конгресс-центры</span></div>
                 </a>
             </div>
             <div class="main-rubricator-cell">
                 <a href="/catalog/place-obrazovatelnye-uchrezhdeniya/" class="main-rubricator-item main-rubricator-item-top main-rubricator-item-white" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Образовательные <br>учреждения</div>
+                    <div class="main-rubricator-item-title"><span>Образовательные <br>учреждения</span></div>
                 </a>
                 <a href="/catalog/place-teatry-i-kinoteatry/" class="main-rubricator-item main-rubricator-item-bottom" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Театры и кинотеатры</div>
+                    <div class="main-rubricator-item-title"><span>Театры и кинотеатры</span></div>
                 </a>
             </div>
             <div class="main-rubricator-cell">
                 <a href="/catalog/place-vokzaly-i-aeroporty/" class="main-rubricator-item main-rubricator-item-top main-rubricator-item-dark" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Вокзалы и аэропорты</div>
+                    <div class="main-rubricator-item-title"><span>Вокзалы и аэропорты</span></div>
                 </a>
                 <a href="/catalog/place-banki/" class="main-rubricator-item main-rubricator-item-bottom main-rubricator-item-white" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Банки</div>
+                    <div class="main-rubricator-item-title"><span>Банки</span></div>
                 </a>
             </div>
             <div class="main-rubricator-cell">
                 <a href="/catalog/place-kafe-i-restorany/" class="main-rubricator-item main-rubricator-item-red" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Кафе и рестораны</div>
+                    <div class="main-rubricator-item-title"><span>Кафе и рестораны</span></div>
                 </a>
             </div>
             <div class="main-rubricator-cell">
                 <a href="/catalog/place-avtosalony/" class="main-rubricator-item main-rubricator-item-white" style="background-image: url();">
-                    <div class="main-rubricator-item-title">Автосалоны</div>
+                    <div class="main-rubricator-item-title"><span>Автосалоны</span></div>
                 </a>
             </div>
         </div>
