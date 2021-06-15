ce89e4541e3a503617876af6acca9602236bed13
Igor <igorekimow@gmail.com>
2021-06-15 18:47:29 +0300

    Fix designer detail

--- local/templates/theexpert/components/bitrix/news/designers/bitrix/news.detail/.default/template.php
+++ local/templates/theexpert/components/bitrix/news/designers/bitrix/news.detail/.default/template.php
@@ -152,7 +152,7 @@ $APPLICATION->IncludeComponent(
 		"SET_STATUS_404" => "Y",
 		"SET_TITLE" => "N",
 		"SHOW_404" => "N",
-	//	"SHOW_ALL_WO_SECTION" => "Y",
+		"SHOW_ALL_WO_SECTION" => "Y",
 		"SHOW_CLOSE_POPUP" => "N",
 		"SHOW_DISCOUNT_PERCENT" => "N",
 		"SHOW_FROM_SECTION" => "N",
