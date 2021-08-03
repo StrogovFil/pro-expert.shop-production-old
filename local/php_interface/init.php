f4a6354ebd9aa6c08e065d4cf7cd8ac3a9d5c677
Margaret <match14@yandex.ru>
2021-08-03 11:11:08 +0300

    deleted preview and detail description from search

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -28,6 +28,27 @@
 //     );
 // }
 
+// Обработчик удаляющий описание из поиска
+AddEventHandler("search", "BeforeIndex", "BeforeIndexHandler");
+
+function BeforeIndexHandler($arFields) {
+   $arrIblock = array(1);   //ID инфоблоков, для которых производить модификацию    
+   $arDelFields = array("DETAIL_TEXT", "PREVIEW_TEXT");    //стандартные поля, которые нужно исключить    
+   if (CModule::IncludeModule('iblock') && $arFields["MODULE_ID"] == 'iblock' && in_array($arFields["PARAM2"], $arrIblock) && intval($arFields["ITEM_ID"])  > 0){
+
+   $dbElement = CIblockElement::GetByID($arFields["ITEM_ID"]);
+       if ($arElement = $dbElement->Fetch()){
+           foreach ($arDelFields as $value){
+               if (isset ($arElement[$value])  && strlen($arElement[$value]) > 0){
+                   $arFields["BODY"] = str_replace (CSearch::KillTags($arElement[$value]) , "", CSearch::KillTags($arFields["BODY"]) );
+               }
+           }
+       }
+
+       return $arFields;
+   }
+} 
+
 function printFilterItemByHTML($filterId = '', $arItem = array())
 {
 	if (!empty($arItem))
